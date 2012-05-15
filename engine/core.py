ed8b733846c6c186742566220854f42091aa6e69
Robert Warner <radicalphoenix@gmail.com>
2012-05-15 15:28:32 -0700

    abstract game core

---
+++ engine/core.py
@@ -0,0 +1,63 @@
+import pygame
+import sys
+from pygame.locals import KEYDOWN, KEYUP, K_ESCAPE
+from engine.render import Renderer
+from engine.controls import KeyBoardController
+from engine.widgets import WidgetHandler, TextWidget
+from engine.utils import quit
+from engine.gameclock import GameClock
+
+
+class GameCore(object):
+
+    def __init__(self,
+                 key_repeat_delay=250,
+                 key_repeat_interval=100,
+                 viewport_size=(800, 600)):
+
+        #initialize pygame
+        pygame.init()
+
+        #create a new renderer
+        self.renderer = Renderer(viewport_size)
+
+        #create a new controller
+        #TODO: refactor controls to be more abstract
+        self.keyboard_controller = KeyBoardController({
+            K_ESCAPE: quit
+        })
+
+        self.keyboard_controller.set_repeat(key_repeat_delay, key_repeat_interval)
+
+        #create a widget handler
+        self.widgets = WidgetHandler()
+
+        #simple widget to show tick
+        fps_display = TextWidget(font_size=40)
+        ups_display = TextWidget(font_size=40)
+        self.widgets.add_widget('fps_display', fps_display, (20, 20))
+        self.widgets.add_widget('ups_display', ups_display, (20, 100))
+
+        if sys.platform in('win32', 'cygwin'):
+            time_source = None
+        else:
+            time_source = lambda: pygame.time.get_ticks() / 1000.
+
+        self.clock = GameClock(max_ups=30,
+                  max_fps=0,
+                  use_wait=False,
+                  update_callback=self._update,
+                  frame_callback=self._draw,
+                  paused_callback=None,
+                  time_source=time_source
+                  )
+
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN or event.type == KEYUP:
+                keystates = pygame.key.get_pressed()
+                self.keyboard_controller.evaluate_keystates(keystates)
+
+    def run(self):
+        while True:
+            self.clock.tick()
--- main.py
+++ main.py
@@ -1,64 +1,7 @@
-import pygame
-import sys
-from pygame.locals import KEYDOWN, KEYUP, K_ESCAPE
-from engine.render import Renderer
-from engine.controls import KeyBoardController
-from engine.widgets import WidgetHandler, TextWidget
-from engine.utils import quit
-from engine.gameclock import GameClock
+from engine.core import GameCore
 
 
-class Game(object):
-    clock = None
-    renderer = None
-
-    def __init__(self):
-        #config settings
-        KEY_REPEAT_DELAY = 250  # how long to wait before resending held down keys
-        KEY_REPEAT_INTERVAL = 100  # how often to resend held down keys
-
-        VIEWPORT_SIZE = (800, 600)
-
-        #initialize pygame
-        pygame.init()
-
-        #create a new renderer
-        self.renderer = Renderer(VIEWPORT_SIZE)
-
-        #create a new controller
-        #TODO: refactor player controls to abstract actions from implementation
-        self.keyboard_controller = KeyBoardController({
-            K_ESCAPE: quit
-        })
-
-        self.keyboard_controller.set_repeat(KEY_REPEAT_DELAY, KEY_REPEAT_INTERVAL)
-
-        #create a widget handler
-        self.widgets = WidgetHandler()
-
-        #simple widget to show tick
-        fps_display = TextWidget(font_size=40)
-        ups_display = TextWidget(font_size=40)
-        self.widgets.add_widget('fps_display', fps_display, (20, 20))
-        self.widgets.add_widget('ups_display', ups_display, (20, 100))
-
-        if sys.platform in('win32', 'cygwin'):
-            time_source = None
-        else:
-            time_source = lambda: pygame.time.get_ticks() / 1000.
-
-        self.clock = GameClock(max_ups=30,
-                  max_fps=0,
-                  use_wait=False,
-                  update_callback=self._update,
-                  frame_callback=self._draw,
-                  paused_callback=None,
-                  time_source=time_source
-                  )
-
-    def run(self):
-        while True:
-            self.clock.tick()
+class Game(GameCore):
 
     def _update(self, interpolation):
         self._handle_events()
@@ -70,11 +13,6 @@ class Game(object):
         self.renderer.reset_view()
         self.renderer.draw_sprite_map(self.widgets.get_widget_map())
         self.renderer.update()
-
-    def _handle_events(self):
-        for event in pygame.event.get():
-            if event.type == KEYDOWN or event.type == KEYUP:
-                keystates = pygame.key.get_pressed()
-                self.keyboard_controller.evaluate_keystates(keystates)
+    
 
 Game().run()
