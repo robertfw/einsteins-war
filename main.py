d9e78101ddec4d6d82859173675c9f47fa310f26
Robert Warner <radicalphoenix@gmail.com>
2012-05-15 14:53:29 -0700

    integrated with gameclock

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
-from pygame.locals import *
-from render import Renderer
-from controls import KeyBoardController
-from widgets import WidgetHandler, TextWidget
-from utils import quit
-from pygame.time import Clock
+import sys
+from pygame.locals import KEYDOWN, KEYUP, K_ESCAPE
+from engine.render import Renderer
+from engine.controls import KeyBoardController
+from engine.widgets import WidgetHandler, TextWidget
+from engine.utils import quit
+from engine.gameclock import GameClock
 
 #config settings
 KEY_REPEAT_DELAY = 250  # how long to wait before resending held down keys
@@ -34,21 +35,36 @@ widget_handler = WidgetHandler()
 fps_display = TextWidget(font_size=40)
 widget_handler.add_widget('fps_display', fps_display, (20, 20))
 
-#create our fps clock
-fps_clock = Clock()
+#create our game clock
+if sys.platform in('win32', 'cygwin'):
+    time_source = None
+else:
+    time_source = lambda: pygame.time.get_ticks() / 1000.
+
+clock = GameClock(ticks_per_second=30,
+                  max_fps=0,
+                  use_wait=False,
+                  max_frame_skip=5,
+                  update_callback=None,
+                  frame_callback=None,
+                  time_source=time_source
+                  )
 
 while True:
-    fps_display.set_text(round(fps_clock.get_fps(), 2))
+    fps_display.set_text(round(clock.get_fps(), 2))
 
-    #TODO: decouple physics tick from renderer tick
-    renderer.reset_view()
-    widgets = widget_handler.get_widget_map()
-    renderer.draw_sprite_map(widgets)
-    renderer.update()
+    clock.tick()
+
+    #TODO: convert this to use callbacks
+    if clock.update_ready:
+        pass
+    elif clock.frame_ready:
+        renderer.reset_view()
+        widgets = widget_handler.get_widget_map()
+        renderer.draw_sprite_map(widgets)
+        renderer.update()
 
     for event in pygame.event.get():
         if event.type == KEYDOWN or event.type == KEYUP:
             keystates = pygame.key.get_pressed()
             keyboard_controller.evaluate_keystates(keystates)
-
-    fps_clock.tick_busy_loop(30)
